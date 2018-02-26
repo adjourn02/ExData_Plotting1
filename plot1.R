ec2517ae9b1a0b163d9f29eae6467d9b04196b3c
dj <dbsalon@up.edu.ph>
2018-02-26 22:10:42 +0800

    added R codes and plots

--- plot1.R
+++ plot1.R
@@ -21,18 +21,3 @@ axis(1, at=seq(0, 3000, by=1000), labels=c("0", "2", "4", "6"))
 axis(2, at=seq(0, 1200, by=200), labels=c("0", "200", "400", "600","800","1000","1200"), cex.axis=0.5)
 dev.copy(png,file="plot1.png")
 dev.off()
-
-## plot 2: line graph of global active power vs. datetime
-plot(data$Date,data$Global_active_power, type="l",xlab="",ylab="Global Active Power (kilowatts)",yaxt='n')
-axis(2, at=seq(0, 3000, by=1000), labels=c("0", "2", "4", "6"))
-dev.copy(png,file="plot2.png")
-dev.off()
-
-## plot 3: lie graph of Energy sub metering
-plot(data$Date,data$Sub_metering_1, type="l",xlab="",ylab="Energy sub metering",yaxt='n')
-lines(data$Date,data$Sub_metering_2, type="l",col="red")
-lines(data$Date,data$Sub_metering_3, type="l",col="blue")
-axis(2, at=seq(0, 30, by=10), labels=c("0", "10", "20", "30"))
-legend("topright",lty=c(1,1,1),col=c("black","red","blue"),cex=c(0.5,0.5,0.5), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-dev.copy(png,file="plot3.png")
-dev.off()
